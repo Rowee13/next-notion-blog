bf125c5c69a8ec225b099a7032e42da0fe044892
Rowee Apor <75659500+Rowee13@users.noreply.github.com>
2025-03-05 16:48:51 +0800

    Initial commit

---
+++ app/(root)/resources/page.tsx
@@ -0,0 +1,13 @@
+const BlocksPage = () => {
+    return (
+        <section>
+            <div className='max-w-7xl mx-auto px-4 py-8 flex flex-col justify-center items-center'>
+                <h1 className='text-3xl font-bold text-center mb-12 text-gray-800 dark:text-white'>
+                    Resources
+                </h1>
+            </div>
+        </section>
+    )
+}
+
+export default BlocksPage
